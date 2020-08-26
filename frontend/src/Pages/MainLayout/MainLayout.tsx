25607c3ad6101bc99a6b664339b08dd201168d52
redhat <ows5ut@gmail.com>
2020-08-26 19:03:23 +0300

    switch on main layout

--- frontend/src/Pages/MainLayout/MainLayout.tsx
+++ frontend/src/Pages/MainLayout/MainLayout.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import './MainLayout.scss';
 import Sidebar from '../../Components/Sidebar';
 import NavBar from '../../Components/NavBar';
+import { Switch, Route } from 'react-router-dom';
 
 const MainLayout: React.FC = () => {
     const [collapsed, collapse] = React.useState<boolean>(false);
@@ -10,6 +11,10 @@ const MainLayout: React.FC = () => {
             <Sidebar/>
             <div className="main">
                 <NavBar/>
+                <div className="p-4">
+                    <Switch>
+                    </Switch>
+                </div>
             </div>
         </div>
     )
