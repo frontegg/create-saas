28e3a29be89df44d96fa3d5ef40fcc511d552e79
redhat <ows5ut@gmail.com>
2020-09-02 13:00:20 +0300

    dashboard fix

--- frontend/src/Components/NavBar/NavBar.tsx
+++ frontend/src/Components/NavBar/NavBar.tsx
@@ -3,9 +3,15 @@ import { Input } from 'reactstrap';
 import './NavBar.scss';
 import ProfileImage from '../ProfileImage';
 
-const NavBar: React.FC = () => {
+const NavBar: React.FC<React.HTMLAttributes<HTMLElement>> = ({className}) => {
     return (
-        <div className="NavBar">
+        <div className={`NavBar ${className}`}>
+            <div className="logo d-none d-md-flex">
+                    <a className="d-flex flex-row align-items-center justify-content-start" href="/">
+                        <img src="/images/logo.png" alt=""/>
+                        <span>Frontegg</span>
+                    </a>
+            </div>
             <label htmlFor="collapsing" className="ml-4">
                 <svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round"  height="20" width="20" xmlns="http://www.w3.org/2000/svg"><line x1="3" y1="12" x2="21" y2="12"></line><line x1="3" y1="6" x2="21" y2="6"></line><line x1="3" y1="18" x2="21" y2="18"></line></svg>
             </label>
--- frontend/src/Pages/MainLayout/MainLayout.tsx
+++ frontend/src/Pages/MainLayout/MainLayout.tsx
@@ -8,13 +8,36 @@ import SliderPage from './SliderPage';
 import NavBar from '../../Components/NavBar';
 
 const MainLayout: React.FC = () => {
+    const [fixedSidebar, setFixSidebar] = React.useState<boolean>(true);
+    const handleFixSidebar = (event: React.ChangeEvent<HTMLInputElement>) => setFixSidebar(!fixedSidebar);
+    const [fixedNavbar, setFixNavbar] = React.useState<boolean>(false);
+    const handlesFixNavbar = (event: React.ChangeEvent<HTMLInputElement>) => setFixNavbar(!fixedNavbar);
+    const [collapsed, collapse] = React.useState<boolean>(false);
+    const handleCollapse = (event: React.ChangeEvent<HTMLInputElement>) => collapse(!collapsed);
+    const [scrolled, setScrolled] = React.useState<boolean>(false);
+    const handleScroll = (event: Event) => {
+            if (window.pageYOffset == 0)
+            {
+                setScrolled(false);
+            }
+            if (window.pageYOffset > 0 && !scrolled) {
+                setScrolled(true);
+            }
+    }
+    React.useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+    
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
 
     return (
-        <div className="mainLayout">
-            <Sidebar/>
-            <div className="main">
-                <NavBar/>
-                <div className="p-4">
+        <div className={`mainLayout ${scrolled ? "scrolled" : "onTop"} ${fixedSidebar ? "fixedSidebar" : ""} ${fixedNavbar ? "fixedNavbar" : ""}`}>
+            <NavBar className={`${collapsed ? "collapsed" : "expanded"} ${fixedNavbar ? "position-fixed" : "position-absolute"}`}/>
+            <Sidebar className={`${collapsed ? "collapsed" : "expanded"}`}/>
+            <div className="main" >
+                <div className="p-4 content">
                     <Switch>
                       <Route path='/forms/sample-forms' component={SampleFormPage} />
                       <Route path='/forms/default-forms' component={DefaultFormPage} />
@@ -22,6 +45,9 @@ const MainLayout: React.FC = () => {
                     </Switch>
                 </div>
             </div>
+            <input type="checkbox" id="collapsing" className="d-none" onChange={handleCollapse} checked={collapsed}/>
+            <input type="checkbox" id="fix_sidebar" className="d-none" onChange={handleFixSidebar} checked={fixedSidebar}/>
+            <input type="checkbox" id="fix_navbar" className="d-none" onChange={handlesFixNavbar} checked={fixedNavbar}/>
         </div>
     )
 }
