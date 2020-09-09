23899466b00a91e9f20e5ae224fb2ffe6a46f6b7
leukhinroman <leukhinroman@gmail.com>
2020-09-09 14:42:11 +0300

    update page Form Steps

--- frontend/src/Components/FormSteps/FormSteps.tsx
+++ frontend/src/Components/FormSteps/FormSteps.tsx
@@ -3,210 +3,118 @@ import * as React from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import './FormSteps.scss';
 
-// type Direction = "circle" | "nested" | "rectangle" | "designation";
-
 type Props = {
     label?:string,
     text_label?: string,
-    // direction?: Direction
 }
 
-const Base:React.FC<Props> = ({
-    label = "Form steps",
-    text_label = "Sample form step components",
-    children }) => {
+const Base:React.FC<Props> = ({ children }) => {
 
-    return <div className="content_step">
-             <div>
+    return <div className="content_step mb-5">
                 <div>
-                    <div className="label">
-                        {label}
-                    </div>
-                    <div className="text-label">
-                        {text_label}
+                    <div className="row">
+                        <div className="col-md-12 mx-0">
+                            {children}
+                        </div>
                     </div>
                 </div>
-                <div className="row">
-                     <div className="col-md-12 mx-0">
-                         {children}
-                     </div>
-                 </div>
-             </div>
             </div>
 }
 
-
-const CircleStep: React.FC<Props> = (props) => {
-    return <Base {...props}>
+export const CircleStep: React.FC<Props> = (props) => {
+    return  <Base {...props}>
                 <div id="msform">
-                            <ul id="progressbar">
-                                <li className="active" id="account"></li>
-                                <li id="personal"></li>
-                                <li id="payment"></li>
-                                <li id="confirm"></li>
-                            </ul>
+                    <ul id="progressbar">
+                        <li className="active" id="account"></li>
+                        <li id="personal"></li>
+                        <li id="payment"></li>
+                        <li id="confirm"></li>
+                    </ul>
                 </div>
             </Base>
 }
 
 export const NestedStep: React.FC<Props> = (props) => {
+
     return <Base {...props}>
         <div id="blockform">
-//                             <div id="blockbar" className="flex fullwidth">
-//                                 <div className="flex stepone fourth-width blue-bg pad">
-//                                     <div className="step w-20">
-//                                         <span className="stepnumber text-color white-bg w-20 height-2 flex">1</span>
-//                                     </div>
-//                                     <div>
-//                                         <div>
-
-//                                         </div>
-//                                         <div>
-
-//                                         </div>
-//                                     </div>
-//                                 </div>
-//                                 <div className="flex steptwo fourth-width blue-bg pad">
-//                                     <div className="step w-20" >
-//                                         <span className="stepnumber text-color white-bg w-20 height-2 flex">2</span>
-//                                     </div>
-//                                     <div>
-//                                         <div>
-
-//                                         </div>
-//                                         <div>
-
-//                                         </div>
-//                                     </div>
-//                                 </div>
-//                                 <div className="flex stepthree fourth-width blue-bg pad">
-//                                     <div className="step w-20">
-//                                         <span className="stepnumber text-color white-bg w-20 height-2 flex">3</span>
-//                                     </div>
-//                                     <div>
-//                                         <div>
-
-//                                         </div>
-//                                         <div>
-
-//                                         </div>
-//                                     </div>
-//                                 </div>
-//                                 <div className="flex stepfour fourth-width blue-bg pad">
-//                                     <div className="step w-20">
-//                                         <span className="stepnumber text-color white-bg w-20 height-2 flex">4</span>
-//                                     </div>
-//                                     <div>
-//                                         <div>
-
-//                                         </div>
-//                                         <div>
-
-                                         </div>
-                                     </div>
-                                 </div>
-                             </div>
-                         </div>
+            <div id="blockbar" className="d-flex w-100">
+                <div className="d-flex stepone w-25 bg-primary pad">
+                    <div className="step align-self-center">
+                        <span className="stepnumber text-black bg-white d-flex justify-content-center align-items-center">1</span>
+                    </div>
+                    <div className="flex-column w-100 ml-3">
+                        <div className="text-white font-weight-bold">
+                            Step 1
+                        </div>
+                        <div className="text-white">
+                            Lorem ipsum dolor
+                        </div>
+                    </div>
+                </div>
+                <div className="d-flex steptwo w-25 pad opacity-25">
+                    <div className="step align-self-center" >
+                        <span className="stepnumber text-white bg-primary d-flex justify-content-center align-items-center">2</span>
+                    </div>
+                    <div className="flex-column w-100 ml-3">
+                        <div className="text-black font-weight-bold">
+                            Step 2
+                        </div>
+                        <div className="text-black">
+                            Lorem ipsum dolor
+                        </div>
+                    </div>
+                </div>
+                <div className="d-flex stepthree w-25 pad opacity-25">
+                    <div className="step align-self-center">
+                        <span className="stepnumber text-white bg-primary d-flex justify-content-center align-items-center">3</span>
+                    </div>
+                    <div className="flex-column w-100 ml-3">
+                        <div className="text-black font-weight-bold">
+                            Step 3
+                        </div>
+                        <div className="text-black">
+                            Lorem ipsum dolor
+                        </div>
+                    </div>
+                </div>
+                <div className="d-flex stepfour w-25 pad opacity-25">
+                    <div className="step align-self-center">
+                        <span className="stepnumber text-white bg-primary d-flex justify-content-center align-items-center">4</span>
+                    </div>
+                    <div className="flex-column w-100 ml-3">
+                        <div className="text-black font-weight-bold">
+                            Step 4
+                        </div>
+                        <div className="text-black">
+                            Lorem ipsum dolor
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </Base>
 }
 
-<form>
-    <CircleStep/>
-    <input></input>
-    <button>submit</button>
-</form>
+export const RectangleStep: React.FC<Props> = (props) => {
+    return <Base {...props}>
+        <div className="designation">
+            <div className="d-flex w-100">
+                <div className="w-25">
+                    <button className="btn btn-default w-100 bg-primary text-white">Step 1</button>
+                </div>
+                <div className="w-25 opacity-25">
+                    <button className="btn btn-default w-100">Step 2</button>
+                </div>
+                <div className="w-25 opacity-25">
+                    <button className="btn btn-default w-100">Step 3</button>
+                </div>
+                <div className="w-25 opacity-25">
+                    <button className="btn btn-default w-100">Step 4</button>
+                </div>
+            </div>
+        </div>
+    </Base>
+}
 
-// const FormSteps: React.FC<Props> = ({ label, text_label, direction}) => {
-
-
-//     return (
-//         <div className="content_step">
-//             <div>
-//                 <div>
-//                     <div className="label">
-//                         {label}
-//                     </div>
-//                     <div className="text-label">
-//                         {text_label}
-//                     </div>
-//                 </div>
-
-//                 <div className="row">
-//                     <div className="col-md-12 mx-0">
-//                         <form id="msform">
-//                             <ul id="progressbar">
-//                                 <li className="active" id="account"></li>
-//                                 <li id="personal"></li>
-//                                 <li id="payment"></li>
-//                                 <li id="confirm"></li>
-//                             </ul>
-//                         </form>
-//                     </div>
-//                 </div>
-//                 <div className="row">
-//                     <div className="col-md-12 mx-0">
-//                         <form id="blockform">
-//                             <div id="blockbar" className="flex fullwidth">
-//                                 <div className="flex stepone fourth-width blue-bg pad">
-//                                     <div className="step w-20">
-//                                         <span className="stepnumber text-color white-bg w-20 height-2 flex">1</span>
-//                                     </div>
-//                                     <div>
-//                                         <div>
-
-//                                         </div>
-//                                         <div>
-
-//                                         </div>
-//                                     </div>
-//                                 </div>
-//                                 <div className="flex steptwo fourth-width blue-bg pad">
-//                                     <div className="step w-20" >
-//                                         <span className="stepnumber text-color white-bg w-20 height-2 flex">2</span>
-//                                     </div>
-//                                     <div>
-//                                         <div>
-
-//                                         </div>
-//                                         <div>
-
-//                                         </div>
-//                                     </div>
-//                                 </div>
-//                                 <div className="flex stepthree fourth-width blue-bg pad">
-//                                     <div className="step w-20">
-//                                         <span className="stepnumber text-color white-bg w-20 height-2 flex">3</span>
-//                                     </div>
-//                                     <div>
-//                                         <div>
-
-//                                         </div>
-//                                         <div>
-
-//                                         </div>
-//                                     </div>
-//                                 </div>
-//                                 <div className="flex stepfour fourth-width blue-bg pad">
-//                                     <div className="step w-20">
-//                                         <span className="stepnumber text-color white-bg w-20 height-2 flex">4</span>
-//                                     </div>
-//                                     <div>
-//                                         <div>
-
-//                                         </div>
-//                                         <div>
-
-//                                         </div>
-//                                     </div>
-//                                 </div>
-//                             </div>
-//                         </form>
-//                     </div>
-//                 </div>
-//             </div>
-//         </div>
-//     )
-
-// }
-
-export default CircleStep;
+// export default CircleStep;
--- frontend/src/Pages/MainLayout/FormStepsPage/FormStepsPage.tsx
+++ frontend/src/Pages/MainLayout/FormStepsPage/FormStepsPage.tsx
@@ -1,19 +1,47 @@
 import * as React from 'react';
-import FormSteps from '../../../Components/FormSteps';
+import {CircleStep, NestedStep, RectangleStep} from '../../../Components/FormSteps';
+
+
 
 const FormStepsPage: React.FC = () => {
 
     return (
-        <div className="formstepspage">
-            <div className="section-title">
-                <div className="category">
-                    FORMS
+        <div>
+            <div className="formstepspage">
+                <div className="section-title">
+                    <div className="category">
+                        FORMS
+                    </div>
+                    <div className="section-name">
+                        Steps
+                    </div>
                 </div>
-                <div className="section-name">
-                    Steps
+                <div>
+                    <div className="">
+                        <div className="label">
+                            Form steps
+                        </div>
+                        <div className="text-label">
+                            Sample form step components
+                        </div>
+                    </div>
+                    <CircleStep />
+                    <NestedStep />
+                    <RectangleStep />
+                </div>
+                <div>
+                    <div className="mb-4">
+                        <div className="label">
+                            Example
+                        </div>
+                        <div className="text-label">
+                            Ready to use form steps example
+                        </div>
+                    </div>
+                    <RectangleStep />
+                    
                 </div>
             </div>
-            <FormSteps label="Form steps" text_label="Sample form step components" />
         </div>
     )
 
