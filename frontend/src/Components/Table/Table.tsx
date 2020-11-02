ae7547b077b68383350ecf668123b409bac13427
livnekes <livnek@gmail.com>
2020-11-02 20:39:46 +0200

    wip

--- frontend/src/Components/Table/Table.tsx
+++ frontend/src/Components/Table/Table.tsx
@@ -66,11 +66,31 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export const Table: React.FC<TableProps> = <T extends object>(props: TableProps<T>) => {
-  const { expandable, selection, pagination } = props;
+  const {
+    expandable,
+    selection,
+    pagination,
+    sortBy,
+    pageCount,
+    data,
+    selectedRowIds,
+    pageSize,
+    rowKey,
+    loading,
+    isMultiSort,
+    renderExpandedComponent,
+    onPageChange,
+    filters: propsFilters,
+    onSortChange: propsOnSortChange,
+    onRowSelected: propsOnRowSelected,
+    onFilterChange: propsOnFilterChange,
+    columns: propsColumns,
+  } = props;
   const classes = useStyles();
   const tableRef = useRef<HTMLTableElement>(null);
+
   const columns = useMemo(() => {
-    const columns = props.columns.map(
+    const columns = propsColumns.map(
       ({ sortable, Filter, Header, ...rest }) =>
         ({
           ...rest,
@@ -115,7 +135,7 @@ export const Table: React.FC<TableProps> = <T extends object>(props: TableProps<
       });
     }
     return columns as Column<T>[];
-  }, [props.columns, expandable]);
+  }, [propsColumns, expandable, classes.checkBox, classes.expandIcon, selection]);
 
   const tableHooks: PluginHook<T>[] = [useFilters, useSortBy];
   if (expandable) {
@@ -148,25 +168,25 @@ export const Table: React.FC<TableProps> = <T extends object>(props: TableProps<
   } = useTable(
     {
       columns,
-      data: props.data,
-      getRowId: (row: any) => row[props.rowKey],
-      manualSortBy: !!props.onSortChange,
-      manualFilters: !!props.onFilterChange,
-      manualPagination: !!props.onPageChange,
-      manualRowSelectedKey: props.rowKey,
-      pageCount: props.pageCount ?? 0,
+      data,
+      getRowId: (row: any) => row[rowKey],
+      manualSortBy: !!propsOnSortChange,
+      manualFilters: !!propsOnFilterChange,
+      manualPagination: !!onPageChange,
+      manualRowSelectedKey: rowKey,
+      pageCount: pageCount ?? 0,
       useControlledState: (state1: any) =>
         ({
           ...state1,
-          sortBy: props.sortBy ?? state1.sortBy,
-          filters: props.filters ?? state1.filters,
-          selectedRowIds: props.selectedRowIds ?? state1.selectedRowIds,
+          sortBy: sortBy ?? state1.sortBy,
+          filters: propsFilters ?? state1.filters,
+          selectedRowIds: selectedRowIds ?? state1.selectedRowIds,
         } as TableState<T> & UseFiltersState<T> & UseSortByState<T> & UseRowSelectState<T>),
       expandSubRows: false,
       initialState: {
         pageIndex: 0,
-        pageSize: props.pageSize ?? 0,
-        selectedRowIds: props.selectedRowIds || {},
+        pageSize: pageSize ?? 0,
+        selectedRowIds: selectedRowIds || {},
       },
     } as UseTableOptions<T> &
       UseFiltersOptions<T> &
@@ -177,19 +197,25 @@ export const Table: React.FC<TableProps> = <T extends object>(props: TableProps<
     ...tableHooks,
   ) as TableInstance<T> & UseTableInstanceProps<T> & UsePaginationInstanceProps<T> & UseRowSelectInstanceProps<T>;
 
-  if (expandable && !props.renderExpandedComponent) {
+  const hasSortBy = props.hasOwnProperty('sortBy');
+  const hasFilters = props.hasOwnProperty('filters');
+  const hasPagination = props.hasOwnProperty('pagination');
+  const hasOnPageChange = props.hasOwnProperty('onPageChange');
+  const hasSelectedRowIds = props.hasOwnProperty('selectedRowIds');
+
+  if (expandable && !renderExpandedComponent) {
     throw Error('Table: you must provide renderExpandedComponent property if the table is expandable');
   }
-  if (props.hasOwnProperty('sortBy') && !props.onSortChange) {
+  if (hasSortBy && !propsOnSortChange) {
     throw Error('Table: you must provide onSortChange property if sortBy is controlled');
   }
-  if (props.hasOwnProperty('filters') && !props.onFilterChange) {
+  if (hasFilters && !propsOnFilterChange) {
     throw Error('Table: you must provide onFilterChange property if filters is controlled');
   }
-  if (props.hasOwnProperty('pagination') && !props.pageSize) {
+  if (hasPagination && !pageSize) {
     throw Error('Table: you must provide pageSize property if pagination enabled');
   }
-  if (props.hasOwnProperty('onPageChange') && !props.pageCount) {
+  if (hasOnPageChange && !pageCount) {
     throw Error('Table: you must provide pageCount property if onPageChange is controlled');
   }
 
@@ -197,86 +223,86 @@ export const Table: React.FC<TableProps> = <T extends object>(props: TableProps<
 
   const onSortChange = useCallback(
     (column: TableColumnProps<T>) => {
-      if (props.hasOwnProperty('sortBy')) {
-        const sortBy = props.isMultiSort ? tableState.sortBy.filter(({ id }) => id !== column.id) : [];
+      if (hasSortBy) {
+        const sortBy = isMultiSort ? tableState.sortBy.filter(({ id }) => id !== column.id) : [];
         if (!column.isSorted) {
           sortBy.push({ id: column.id, desc: false });
         } else if (!column.isSortedDesc) {
           sortBy.push({ id: column.id, desc: true });
         }
-        props.onSortChange?.(sortBy);
+        propsOnSortChange?.(sortBy);
       } else {
         if (column.isSorted && column.isSortedDesc) {
           column.clearSortBy();
         } else {
-          column.toggleSortBy(column.isSorted, props.isMultiSort ?? false);
+          column.toggleSortBy(column.isSorted, isMultiSort ?? false);
         }
       }
     },
-    [props.onSortChange],
+    [hasSortBy, isMultiSort, tableState.sortBy, propsOnSortChange],
   );
 
   const onFilterChange = useCallback(
     (column: TableColumnProps<T>, filterValue?: any) => {
-      if (props.hasOwnProperty('filters')) {
+      if (hasFilters) {
         const filters = tableState.filters.filter(({ id }) => id !== column.id);
         if (filterValue != null) {
           filters.push({ id: column.id, value: filterValue });
         }
-        props.onFilterChange?.(filters);
+        propsOnFilterChange?.(filters);
       } else {
         column.setFilter(filterValue);
       }
     },
-    [props.onFilterChange],
+    [propsOnFilterChange, hasFilters, tableState.filters],
   );
 
   const onToggleAllRowsSelected = useCallback(
     (value: boolean) => {
-      if (props.hasOwnProperty('selectedRowIds')) {
-        const selectedIds = props.data.reduce((p, n: any) => ({ ...p, [n[props.rowKey]]: true }), {});
-        props.onRowSelected?.(value ? selectedIds : {});
+      if (hasSelectedRowIds) {
+        const selectedIds = data.reduce((p, n: any) => ({ ...p, [n[rowKey]]: true }), {});
+        propsOnRowSelected?.(value ? selectedIds : {});
       } else {
         toggleAllRowsSelected(value);
       }
     },
-    [props.onRowSelected],
+    [hasSelectedRowIds, data, rowKey, propsOnRowSelected, toggleAllRowsSelected],
   );
 
   const onRowSelected = useCallback(
     (row: any, value: boolean) => {
-      const id = row[props.rowKey];
-      if (props.hasOwnProperty('selectedRowIds')) {
-        const newSelectedRows: any = { ...props.selectedRowIds };
+      const id = row[rowKey];
+      if (hasSelectedRowIds) {
+        const newSelectedRows: any = { ...selectedRowIds };
         if (value) {
           newSelectedRows[id] = true;
         } else {
           delete newSelectedRows[id];
         }
-        props.onRowSelected?.(newSelectedRows);
+        propsOnRowSelected?.(newSelectedRows);
       } else {
         toggleRowSelected(id, value);
       }
     },
-    [props.onRowSelected],
+    [hasSelectedRowIds, rowKey, selectedRowIds, toggleRowSelected, propsOnRowSelected],
   );
 
   useEffect(() => {
-    if (!props.hasOwnProperty('sortBy')) props.onSortChange?.(tableState.sortBy);
-  }, [props.sortBy, tableState.sortBy]);
+    if (!hasSortBy) propsOnSortChange?.(tableState.sortBy);
+  }, [hasSortBy, propsOnSortChange, tableState.sortBy]);
 
   useEffect(() => {
-    if (!props.hasOwnProperty('filters')) props.onFilterChange?.(tableState.filters);
-  }, [props.filters, tableState.filters]);
+    if (!hasFilters) propsOnFilterChange?.(tableState.filters);
+  }, [hasFilters, propsOnFilterChange, tableState.filters]);
 
   useEffect(() => {
     tableRef.current?.querySelector('.table-tbody')?.scroll?.({ top: 0, left: 0, behavior: 'smooth' });
-    props.onPageChange?.(tableState.pageSize, tableState.pageIndex);
-  }, [tableState.pageIndex]);
+    onPageChange?.(tableState.pageSize, tableState.pageIndex);
+  }, [onPageChange, tableState.pageSize, tableState.pageIndex]);
 
   useEffect(() => {
-    if (!props.hasOwnProperty('selectedRowIds')) props.onRowSelected?.(tableState.selectedRowIds as any);
-  }, [tableState.selectedRowIds]);
+    if (!hasSelectedRowIds) propsOnRowSelected?.(tableState.selectedRowIds as any);
+  }, [hasSelectedRowIds, propsOnRowSelected, tableState.selectedRowIds]);
 
   const onPageChangeHandler = (page: number) => {
     if (page > tableState.pageIndex) {
@@ -300,9 +326,9 @@ export const Table: React.FC<TableProps> = <T extends object>(props: TableProps<
         <TableBody
           getTableBodyProps={getTableBodyProps}
           prepareRow={prepareRow}
-          loading={props.loading}
+          loading={loading}
           rows={(pagination ? page : rows) as (Row<T> & UseExpandedRowProps<T>)[]}
-          renderExpandedComponent={props.renderExpandedComponent}
+          renderExpandedComponent={renderExpandedComponent}
         />
       </MaterialUITable>
       {pagination === 'pages' && (
